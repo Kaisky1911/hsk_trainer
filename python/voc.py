61c560b695e33368c34a9adaf7fe15498244b878
Kaisky <kai.mueller1996@gmail.com>
2022-08-26 00:37:54 +0200

    blabal

--- python/voc.py
+++ python/voc.py
@@ -1,11 +1,11 @@
 
 import json
 
-hsk = open("./hsk.txt").read()
+hsk = open("./hsk.txt", encoding="utf8").read()
 hsk_lines = hsk.split("\n")
 print(len(hsk_lines))
 
-dict_data = open("./dict.txt").read()
+dict_data = open("./dict.txt", encoding="utf8").read()
 dict_lines = dict_data.split("\n")
 print(len(dict_lines))
 
@@ -50,30 +50,56 @@ def pinyin_convert(s):
 
 for w in hsk_lines:
     if w in d:
-        status = "goood"
+        status = "ok"
         if len(d[w]) > 1:
-            status = "???"
+            status = "problem 1: more than one meaning"
         translations = []
+        liangci = None
         for line in d[w]:
             translation = {
-                "line": line,
+                "raw": line,
             }
             pinyin_start = line.find("[") + 1
             pinyin_end = line.find("]")
             translation["pinyin"] = pinyin_convert(line[pinyin_start:pinyin_end])
+            translation["english"] = line.split("/")[1:-1]
+            if translation["english"][-1][:3] == "CL:":
+                cls = translation["english"][-1][3:].split(",")
+                new_transl = ""
+                for cl in cls:
+                    split_pos = cl.find("|")
+                    if split_pos >= 0:
+                        cl = cl[split_pos+1:]
+                    py_start = cl.find("[")
+                    py_tokens = pinyin_convert(cl[py_start:-1])
+                    cl = cl[:py_start]
+                    for t in py_tokens:
+                        cl += t
+                    cl += "]"
+                    new_transl += cl + ","
+                translation["english"] = translation["english"][:-1]
+                liangci = new_transl[:-1]
+            if status == "ok" and len(translation["english"]) > 5:
+                status = "problem 2: more than five translations"
+
+
             translations.append(translation)
-        hsk_dict.append({
+        entry = {
             "hanzi": w,
             "translations": translations,
             "status": status,
-        })
+        }
+        if liangci:
+            entry["liangci"] = liangci
+        hsk_dict.append(entry)
     else:
         hsk_dict.append({
+            "hanzi": w,
             "translations": [],
-            "status": "???",
+            "status": "problem 0: no translation",
         })
 
 
 
 
-json.dump(hsk_dict, open("hsk.json", "w"), indent=4, sort_keys=True, ensure_ascii=False)
+json.dump(hsk_dict, open("hsk.json", "w", encoding="utf8"), indent=4, sort_keys=True, ensure_ascii=False)
